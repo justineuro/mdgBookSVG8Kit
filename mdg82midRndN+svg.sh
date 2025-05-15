61632fbc5214aa1db58a8b40ce2ac5a7a0777108
Justine Leon Uro <justineuro@gmail.com>
2025-05-15 14:56:40 +0800

    2025-05-15

--- mdg82midRndN+svg.sh
+++ mdg82midRndN+svg.sh
@@ -12,14 +12,14 @@
 #		of a particular Musical Dice Game (MDG) minuet based on Ludus Melothedicus
 #
 #      AUTHOR:	J.L.A. Uro (justineuro@gmail.com)
-#     VERSION:	0.0.0
+#     VERSION:	0.0.1
 #     LICENSE:	Creative Commons Attribution 4.0 International License (CC-BY)
 #     CREATED:	2025/05/06 03:50:24
-#    REVISION:	
+#    REVISION:	2025/05/15 12:46:25
 #==================================================================================
 
 #----------------------------------------------------------------------------------
-# define the function genS() that randomly chooses an integer from 1 to 6, inclusive
+# define the function genS() that randomly chooses an integer from 1 to 9, inclusive
 #----------------------------------------------------------------------------------
 genS() { # RANDOM randomly generates an integer from 0 to 32765
 	rnd=32759
@@ -43,15 +43,10 @@ i=1
 while [ $i -le $1 ]; do
 
 #----------------------------------------------------------------------------------
-# generate the random 32-sequence of outcomes of the 32 throws of a dice for 
-# the measures of the rondo:
-#		n1-32 are any of the 11 possible integers from the set {2,3,4,..., 10,11,12}; 
-#		n1-n8 for 1st part of the aria (measures from Table 1, cols 1-8)
-#		n9-n12 for ritornello (measures for the Table at the end of the Series)
-#		n13-n20 for the 2nd part of the aria (measures from Table 2) 
-#		n21-n28 for the 3rd part of the aria (measures from Table 3)
-#		n29-n32 for the additional 4 measures of the optional 4th part of the aria
-#							(measures from Tables 1, cols 9-12)
+# generate the random 16-sequence of outcomes of the 16 throws of a 9-sided die/top  
+# for the measures of the minuet:
+#		n1-n8 for the 1st part 
+#		n9-n16 for the 2nd part
 #----------------------------------------------------------------------------------
 	for j in {0..15}; do
 		diceR[$j]=`genS`
@@ -59,8 +54,8 @@ while [ $i -le $1 ]; do
 echo ${diceR[*]}
 	
 #----------------------------------------------------------------------------------
-# generate a rondo in ABC notation and corresponding MIDI and svg for the current 
-# diceR using mdg72mid+svg.sh
+# generate a minuet in ABC notation and corresponding MIDI and svg for the current 
+# diceR using mdg82mid+svg.sh
 #----------------------------------------------------------------------------------
 	./mdg82mid+svg.sh ${diceR[*]}
 	i=`expr $i + 1`
